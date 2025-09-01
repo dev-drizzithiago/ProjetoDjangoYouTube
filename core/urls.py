3b712b2171fc138c044bf99a33bf3a408a8237f7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 08:30:33 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
-urlpatterns = (
-    path('', views.index, name='index')
-)
+from .views import index
+
+urlpatterns = [
+    path('', index, name='index')
+]
