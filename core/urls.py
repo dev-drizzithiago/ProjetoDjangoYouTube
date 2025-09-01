dd8677cc78f74af139f1ab360999c7012650ae9d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 20:42:41 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import index, add_link_sistema
 
 urlpatterns = [
     path('', index, name='index'),
     path('add_link_sistema/', add_link_sistema, name='add_link_sistema')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
