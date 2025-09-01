3c6a3e086f729cb9bf6a1acf60507f9f77d23e0e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 12:01:21 -0300

    Update script.js

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,11 +1,37 @@
-import { btn_index } from './utilitys.js';
+import { btn_index, getCookie } from './utilitys.js';
 
 
 
+function add_link_sistema (link_youtube) {
+    const response = fetch("/add_link_sistema/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify(link_youtube)
+    })
+
+    const data = response.json();
+    console.log(data);
+}
+
+
+class btn_youtube  {
+    constructor(link) {
+        this.link = link;
+    }
+
+    add_link() {
+        add_link_sistema(this.link);
+    }
+}
 
 
 btn_index.btn_adicionar.addEventListener('click', () => {
-    console.log('Adicionar button clicked');
+    const link = document.getElementById('id_input_link').value;
+    const btn = new btn_youtube(link);
+    btn.add_link();
 });
 
 btn_index.btn_download.addEventListener('click', () => {
