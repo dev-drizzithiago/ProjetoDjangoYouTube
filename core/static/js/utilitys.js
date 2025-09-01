d22a0311c2c4ab22ed03cdfe2f5be3b7166c1a17
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 10:56:08 -0300

    Create utilitys.js

---
+++ core/static/js/utilitys.js
@@ -0,0 +1,7 @@
+
+export const btn_index = {
+    btn_adicionar: document.getElementById('add_link')
+    btn_adicionar: document.getElementById('add_link')
+    btn_adicionar: document.getElementById('add_link')
+    btn_adicionar: document.getElementById('add_link')
+}
