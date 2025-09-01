7dde0550cdd34564cf32bbfe1708fa3bcce36aac
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 12:01:27 -0300

    Update utilitys.js

--- core/static/js/utilitys.js
+++ core/static/js/utilitys.js
@@ -1,7 +1,23 @@
 
 export const btn_index = {
     btn_adicionar: document.getElementById('id_btn_add_link'),
-    btn_adicionar: document.getElementById('id_btn_download'),
-    btn_adicionar: document.getElementById('id_btn_remover'),
-    btn_adicionar: document.getElementById('id_btn_player'),
+    btn_download: document.getElementById('id_btn_download'),
+    btn_remover: document.getElementById('id_btn_remover'),
+    btn_player: document.getElementById('id_btn_player'),
+}
+
+// Função para "autenticar" o django.
+export function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i=0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break
+            }
+        }
+    }
+    return cookieValue;
 }
