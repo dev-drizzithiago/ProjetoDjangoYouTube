4b275dde89e712adfafcf236670104b299879f9a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 12:01:35 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
+import json
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+def add_link_sistema(request):
+    dados_json = json.loads(request.body)
+    print(dados_json)
