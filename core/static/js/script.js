65b80c174068835718199e8d74b4882f3a26e35c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 11:05:31 -0300

    Update script.js

---
+++ core/static/js/script.js
@@ -0,0 +1,17 @@
+import { btn_index } from './utilitys.js';
+
+btn_index.btn_adicionar.addEventListener('click', () => {
+    console.log('Adicionar button clicked');
+});
+
+btn_index.btn_download.addEventListener('click', () => {
+    console.log('Download button clicked');
+});
+
+btn_index.btn_remover.addEventListener('click', () => {
+    console.log('Remover button clicked');
+});
+
+btn_index.btn_player.addEventListener('click', () => {
+    console.log('Player button clicked');
+});
