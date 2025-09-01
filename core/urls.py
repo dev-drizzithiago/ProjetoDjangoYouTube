016841a93d041f9387ae3ca7a925b958aa99ddff
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 12:01:30 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, add_link_sistema
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('add_link_sistema/', add_link_sistema, name='add_link_sistema')
 ]
