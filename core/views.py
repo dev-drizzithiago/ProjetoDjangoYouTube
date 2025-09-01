c4a083e62c8462e1291a343a1374994e01ecf6fd
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 20:43:51 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -3,16 +3,18 @@ import os.path
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.conf import settings
 
+midia = 'Dreams (2004 Remaster).mp4'
 
 # Create your views here.
-MIDIA_LOCAL = os.path.join()
+MIDIA_LOCAL = os.path.join(settings.MEDIA_URL, 'movies', midia)
 def index(request):
-    midia_local =
+
     context = {
-        'midia_local': midia_local
+        'midia_local': MIDIA_LOCAL.replace('\\', '/')
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 
 def add_link_sistema(request):
