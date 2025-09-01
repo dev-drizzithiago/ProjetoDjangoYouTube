191f069ae170f0329966405433d3476d0936c49b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-01 11:05:35 -0300

    Update utilitys.js

--- core/static/js/utilitys.js
+++ core/static/js/utilitys.js
@@ -1,7 +1,7 @@
 
 export const btn_index = {
-    btn_adicionar: document.getElementById('add_link')
-    btn_adicionar: document.getElementById('add_link')
-    btn_adicionar: document.getElementById('add_link')
-    btn_adicionar: document.getElementById('add_link')
+    btn_adicionar: document.getElementById('id_btn_add_link'),
+    btn_adicionar: document.getElementById('id_btn_download'),
+    btn_adicionar: document.getElementById('id_btn_remover'),
+    btn_adicionar: document.getElementById('id_btn_player'),
 }
