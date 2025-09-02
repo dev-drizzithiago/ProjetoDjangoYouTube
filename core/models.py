a30f392285f8f5df615fcd98513bbf7bb91470b3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-02 10:19:00 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Base(models.Model):
+    data_criacao = models.DateField(auto_created=True)
+class DadosYtoutube(Base):
+    autor_link = models.CharField(max_length=255)
+    titulo_link = models.CharField(max_length=255)
+    duracao_link = models.IntegerField()
+    miniatura_link = models.CharField(max_length=255)
+    download_link = models.CharField(max_length=255)
