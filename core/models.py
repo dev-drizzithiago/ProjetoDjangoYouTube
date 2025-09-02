a02684babff1090bb77163a64f3b981f46bc443d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-02 12:42:30 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -3,10 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Base(models.Model):
-    data_criacao = models.DateField(auto_created=True)
+    data_criacao = models.DateField('data_criacao', auto_now_add=True)
+
 class DadosYoutube(Base):
     autor_link = models.CharField(max_length=255)
     titulo_link = models.CharField(max_length=255)
-    duracao_link = models.IntegerField()
-    miniatura_link = models.CharField(max_length=255)
-    download_link = models.CharField(max_length=255)
+    duracao = models.IntegerField()
+    miniatura = models.CharField(max_length=255)
+    link_tube = models.CharField(max_length=255)
