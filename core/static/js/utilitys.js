81ab7479881a7c11689cdc5ae00919055a586c88
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-05 18:59:46 -0300

    Update utilitys.js

--- core/static/js/utilitys.js
+++ core/static/js/utilitys.js
@@ -45,3 +45,31 @@ export function converterDuracao(duracao) {
     const segundos = duracao % 60;  // converte o valor inteiro para segundos.
     return `${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
 }
+
+
+/*
+function carregaPagina(response) {
+    const elemento = document.querySelector('.div_resultado_link');  // Elemento onde os links serão carregados
+    const lista = document.createElement('ul');  // Cria uma lista não ordenada, conterá os links
+    elemento.appendChild(lista);  // cria a lista dentro do elemento
+
+    response.forEach(item => {
+        const li = document.createElement('li');  // cria um item de lista, onde vai conter cada dado
+        const img = document.createElement('img'); // cria um elemento de imagem 
+        const tm = document.createElement('p')
+
+        li.style.marginTop = '15px'; // define a margem superior da imagem
+
+        img.style.width = '90px'; // define a largura da imagem
+        img.style.height = '60px'; // define a altura da imagem
+        img.style.display = 'grid'; // define o modo de exibição da imagem
+
+        li.textContent = item.autor_link;  // define o texto do item de lista
+        img.src = item.miniatura;  // define a fonte da imagem
+        tm.textContent = converterDuracao(item.duracao);  // define o texto do item de lista
+       
+        lista.appendChild(li);  // adiciona o item de lista à lista
+        li.appendChild(img); // adiciona a imagem ao item de lista
+        li.appendChild(tm); // adiciona a duração ao item de lista
+    });
+}*/
