186e904ef175138110072578b34164b1f252f12c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-06 19:19:12 -0300

    Update settings.py

--- DjangoYouTube/settings.py
+++ DjangoYouTube/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = "static/"
 STATIC_ROORT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
-media_root = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
