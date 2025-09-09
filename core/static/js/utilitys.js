9bef5878bb3dcecb1d36bef815e69a7eb8f324c0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-09 20:45:36 -0300

    Update utilitys.js

--- core/static/js/utilitys.js
+++ core/static/js/utilitys.js
@@ -10,6 +10,7 @@ export const elemento_index = {
     msg_alerta: document.getElementById('id_msg_alerta'),
     link_entrada: document.getElementById('id_input_link'),
     div_result: document.querySelector('.content'),
+    modalOpcMidia: document.querySelector('.modalOpcMidia')
 }
 
 // Função para "autenticar" o django.
