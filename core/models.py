61a271473d4c73d38c0298d831cad34a6791236f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-13 12:22:15 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -9,6 +9,7 @@ class Base(models.Model):
     data_criacao = models.DateField('data_criacao', auto_now_add=True)
 
 class DadosYoutube(Base):
+    id_link = models.IntegerField(primary_key=True, unique=True)
     autor_link = models.CharField(max_length=255)
     titulo_link = models.CharField(max_length=255)
     duracao = models.IntegerField()
@@ -16,7 +17,9 @@ class DadosYoutube(Base):
     link_tube = models.CharField(max_length=255)
 
 class MoviesSalvasServidor(Base):
+    id_miniatura = models.IntegerField(primary_key=True, unique=True)
     nome_arquivo = models.CharField(max_length=255, null=True)
     path_arquivo = models.CharField(max_length=255, null=True)
     duracao_midia = models.IntegerField()
     path_miniatura = models.FileField(upload_to='miniaturas/', max_length=255)
+    id_dados_yt = models.ForeignKey(DadosYoutube)
