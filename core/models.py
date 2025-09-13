af5338ab49487f5f5d5f75091f30ca681c1cccf0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-13 12:36:59 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ import os.path
 
 from django.db import models
 from django.conf import settings
-from django.db.models import CASCADE
+from django.db.models import CASCADE, PROTECT
 
 
 # Create your models here.
@@ -11,6 +11,7 @@ class Base(models.Model):
     data_criacao = models.DateField('data_criacao', auto_now_add=True)
 
 class DadosYoutube(Base):
+    id_dados_yt = models.IntegerField(primary_key=True)
     autor_link = models.CharField(max_length=255)
     titulo_link = models.CharField(max_length=255)
     duracao = models.IntegerField()
@@ -18,8 +19,9 @@ class DadosYoutube(Base):
     link_tube = models.CharField(max_length=255)
 
 class MoviesSalvasServidor(Base):
+    id_dados_yt = models.IntegerField(primary_key=True)
     nome_arquivo = models.CharField(max_length=255, null=True)
     path_arquivo = models.CharField(max_length=255, null=True)
     duracao_midia = models.IntegerField()
     path_miniatura = models.FileField(upload_to='miniaturas/', max_length=255)
-    dados_youtube = models.ForeignKey(DadosYoutube, on_delete=CASCADE, null=False, blank=False)
+    dados_youtube = models.ForeignKey(DadosYoutube, on_delete=PROTECT)
