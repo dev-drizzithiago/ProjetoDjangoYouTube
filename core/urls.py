017164d1456595be65ab72b6822a8e5c3e38b320
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-09-16 21:43:31 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index, add_link_sistema, links_salvos, player_midias_mp4, download_link, player_midias_mp3
+from .views import (
+    index, add_link_sistema, links_salvos,
+    player_midias_mp4, download_link, player_midias_mp3, login)
 
 urlpatterns = [
 
@@ -11,6 +13,7 @@ urlpatterns = [
     path('player_midias_mp4/', player_midias_mp4, name='player_midias_mp4'),
     path('player_midias_mp3/', player_midias_mp3, name='player_midias_mp3'),
     path('download_link/', download_link, name='download_link'),
+    path('login/', login, name='login'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
